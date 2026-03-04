4f669f8fccd1a14fd0d548693eb455023b201a55
tomascimmino <tomascimmino@gmail.com>
2026-03-04 17:25:24 -0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='sokos-v1';
+const ASSETS=['./index.html'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
